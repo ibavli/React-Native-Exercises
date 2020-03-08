84b3a10977b49c5c1e7cd3a179cf90e07ea3e40f
ibrahim BAVLI <31312276+ibavli@users.noreply.github.com>
2020-03-08 23:55:49 +0300

    button example cont

--- src/components/ButtonExample.js
+++ src/components/ButtonExample.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react'
-import { View, Button, StyleSheet } from "react-native";
+import { View, Button, StyleSheet, Text, Alert } from "react-native";
 
+function Separator() {
+    return <View style={styles.separator} />;
+}
 
 export default class ButtonExample extends Component {
 
     state = {
-        buttonName: "Click me!"
+        buttonName: "Click me!",
+        textDescription: ''
     }
 
     changeName = () => {
@@ -14,13 +18,52 @@ export default class ButtonExample extends Component {
         })
     }
 
+    showDescription = () => {
+        this.setState({
+            textDescription: 'Surpise !'
+        })
+    }
+
     render() {
         return (
             <View style={styles.container}>
+
                 <Button
                     onPress={() => this.changeName()}
                     title={this.state.buttonName}
                     color="#f194ff" />
+
+                <Separator />
+
+                <Button
+                    onPress={() => this.showDescription()}
+                    title='Click me!' />
+
+                <Separator />
+
+                <Text style={styles.text}>
+                    {this.state.textDescription}
+                </Text>
+
+                <Separator />
+
+                <Button
+                    onPress={() => this.showDescription()}
+                    disabled
+                    title='Disabled button' />
+
+                <Separator />
+
+                <View style={styles.fixToText}>
+                    <Button
+                        title="Left button"
+                        onPress={() => Alert.alert('Alert Title','Left button pressed')}
+                    />
+                    <Button
+                        title="Right button"
+                        onPress={() => Alert.alert('Alert Title','Right button pressed')}
+                    />
+                </View>
             </View>
         )
     }
@@ -32,4 +75,20 @@ const styles = StyleSheet.create({
         margin: 15
     },
 
-})
+    separator: {
+        marginVertical: 20,
+        borderBottomColor: '#737373',
+        borderBottomWidth: StyleSheet.hairlineWidth,
+    },
+
+    text: {
+        textAlign: 'center',
+        fontSize: 20,
+        color: '#33B8FF'
+    },
+
+    fixToText: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+    }
+});
