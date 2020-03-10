a1346010284ef41117c4fa1af6e1bce8ae463322
ibrahim BAVLI <31312276+ibavli@users.noreply.github.com>
2020-03-10 23:43:16 +0300

    Modal example cont

--- src/components/ModalExample.js
+++ src/components/ModalExample.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
-import { View, Modal, Alert, Text, TouchableHighlight } from "react-native";
+import { View, Modal, Alert, Text, TouchableHighlight, Button } from "react-native";
 
 
 export default class ModalExample extends Component {
 
     state = {
-        modalVisible: false
+        modalVisible: false,
+        modalText: "   Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum"
     };
 
     setModalVisible(visible) {
@@ -25,25 +26,31 @@ export default class ModalExample extends Component {
 
                     <View style={{ marginTop: 22 }}>
                         <View>
-                    
-                            <Text style={{backgroundColor: "deeppink"}}>Hello World!</Text>
+
+                            <Text style={{ backgroundColor: "antiquewhite ", padding: 15 }}>{this.state.modalText}</Text>
                             <TouchableHighlight
                                 onPress={() => {
                                     this.setModalVisible(!this.state.modalVisible);
                                 }}>
-                                <Text>Hide Modal</Text>
+                                <Button
+                                    title="Close Modal"
+                                    onPress={() => {
+                                        this.setModalVisible(!this.state.modalVisible);
+                                    }}
+                                />
                             </TouchableHighlight>
                         </View>
                     </View>
 
                 </Modal>
 
-                <TouchableHighlight
+                <Button
                     onPress={() => {
-                        this.setModalVisible(true);
-                    }}>
-                    <Text>Show Modal</Text>
-                </TouchableHighlight>
+                        this.setModalVisible(!this.state.modalVisible);
+                    }}
+                    title="Show Modal"
+                    style={{ padding: 15 }}
+                />
             </View>
         );
     }
